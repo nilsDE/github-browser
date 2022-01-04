42858b4d9dc57e036fb01fd520c4aa1a13aefd44
Nils <nils.rohe@sosafe.de>
2022-01-04 20:15:41 +0100

    fix authentication

--- src/axios.ts
+++ src/axios.ts
@@ -6,9 +6,9 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use((config) => {
     const newConfig = config;
-    newConfig.params = {
-        client_id: process.env.REACT_APP_GH_CLIENT_ID,
-        client_secret: process.env.REACT_APP_GH_CLIENT_SECRET,
+    newConfig.auth = {
+        username: process.env.REACT_APP_GH_CLIENT_ID,
+        password: process.env.REACT_APP_GH_CLIENT_SECRET,
         ...config.params,
     };
     return newConfig;
